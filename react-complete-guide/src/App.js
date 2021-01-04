f5dbf1beb2f9c6aec05ff638b33ec3bb103cca35
Dhita Irma <dhita.irma@gmail.com>
2021-01-04 10:49:03 +0800

    Use props, create state, and setState

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,12 +3,35 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: '28'},
+      {name: 'Manu', age: '29'},
+      {name: 'Dhita', age: '25'},
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked');
+    // DONT DO THIS this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        {name: 'Max', age: '28'},
+        {name: 'Manu', age: '29'},
+        {name: 'Tsuki', age: '27'},
+      ]
+    })
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm Dhita and I'm learning React</h1>
         <p>This is really working!</p>
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 
-const person = () => {
-    return <p>I'm a Person!</p>
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
