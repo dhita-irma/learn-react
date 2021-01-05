ea503259a1b694b7f73137e263465f9f39870e7b
Dhita Irma <dhita.irma@gmail.com>
2021-01-05 15:28:46 +0800

    Style components: Stylesheet & Inline style

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
+
 import './App.css';
+import './Person/Person.css';
 
 class App extends Component {
   state = {
@@ -35,11 +37,23 @@ class App extends Component {
   }
   
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm Dhita and I'm learning React</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button> 
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Maximilian!!')}
+          >Switch Name</button> 
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}/>
