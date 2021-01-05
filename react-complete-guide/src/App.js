07da8d5cbb3f3e18b4533efa8aa4bb831cea7c60
Dhita Irma <dhita.irma@gmail.com>
2021-01-05 15:11:04 +0800

    Two-way binding

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,14 +11,25 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked');
     // DONT DO THIS this.state.persons[0].name = 'Maximilian';
     this.setState({
       persons: [
-        {name: 'Max', age: '28'},
+        {name: newName, age: '28'},
         {name: 'Manu', age: '29'},
         {name: 'Tsuki', age: '27'},
+      ]
+    })
+  }
+
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Max', age: '28'},
+        {name: event.target.value, age: '29'},
+        {name: 'Dhita', age: '25'},
       ]
     })
   }
@@ -28,10 +39,18 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm Dhita and I'm learning React</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button> 
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler}> My Hobby: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
