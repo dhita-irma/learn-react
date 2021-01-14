61ac7f1be5c5bd6539ffbfa106fe55e6e85d1a57
Dhita Irma <dhita.irma@gmail.com>
2021-01-14 22:51:19 +0800

    Create Form & TodoList component and add style

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
+import Form from './components/Form'
+import TodoList from './components/TodoList';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React!</h1>
+      <header>
+        <h1>Dhita's Todo List</h1>
+      </header>
+      <Form setInputText={setInputText}/>
+      <TodoList />
     </div>
   );
 }
