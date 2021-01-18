55e1cae738b16be8cd659f9daaad844cde6d2b9e
Dhita Irma <dhita.irma@gmail.com>
2021-01-18 23:00:17 +0800

    Create Menu Items components

--- 02-menu/src/App.js
+++ 02-menu/src/App.js
@@ -1,10 +1,31 @@
 import React, { useState } from 'react';
+
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+      </section>
+      <Categories filterItems={filterItems}/>
+      <Menu items={menuItems}/>
+    </main>
+  );
 }
 
 export default App;
