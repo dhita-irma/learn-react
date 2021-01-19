fbac66811fb9a58d2b5f4b3a6ee8102f67ee4f93
Dhita Irma <dhita.irma@gmail.com>
2021-01-19 15:54:06 +0800

    Use map and set to get all categories

--- 02-menu/src/App.js
+++ 02-menu/src/App.js
@@ -5,22 +5,12 @@ import Categories from './Categories';
 import items from './data';
 
 
+const allCategories = ['all', ...new Set(items.map(item => item.category))];
+console.log(allCategories);
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-
-    // Set categories 
-    var menuCategories = ['all'];
-    for (var i = 0; i < items.length; i++) {
-      if (!menuCategories.includes(items[i].category)) {
-        menuCategories.push(items[i].category);
-      }
-    }
-
-    setCategories(menuCategories);
-  }, []);
+  const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
       if (category === 'all') {
