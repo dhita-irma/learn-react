e1ccf42d59682ce60bfbf8d42bfe3b4e8a4da1c3
Dhita Irma <dhita.irma@gmail.com>
2021-01-28 22:16:22 +0800

    Rename directory

---
+++ 03-burger/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Items from './components/Items'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Hello world!</h1>
+      </header>
+      <Items />
+    </div>
+  );
+}
+
+export default App;
---
+++ 03-burger/src/components/Items.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const items = () => {
+    return(
+        <h2>This is item sections</h2>
+    );
+};
+
+export default items;
